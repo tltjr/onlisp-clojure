3df9c819b6a4ebed2a10457d1b84bbccbdbd87ab
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-01-05 15:13:35 -0700

    Remove renamed files

--- src/onlisp_clojure/ch3.clj
+++
@@ -1,9 +0,0 @@
-;; 3.1
-;; Functional Design
-
-(defn good-reverse [coll]
-  (defn rev [coll acc]
-    (if (empty? coll)
-      acc
-      (rev (rest coll) (cons (first coll) acc))))
-  (rev coll nil))
